1b5f13d87f28535237f61de86a66f213b4000e57
qunwang <niuyiwangqun@sohu.com>
2017-02-05 23:29:09 +0800

    修改模块的接口

--- index.js
+++ index.js
@@ -3,19 +3,56 @@
  */
 
 var sprite = require( "./lib/css-img-sprite" );
-
+var fs = require( "fs" );
+var path = require( "path" );
 /**
  *  arguments:
  *      content:{buffer} css file content
- *      obj:{object}
- *          obj.cssSrc:{string} although you give content,we still need file name,so,give us cssSrc
- *          [obj.cssDesDir]:{string} css output dir ,default:cssSrc.we do not write new css file for you,you need do it yourself.we need it because we need to change css background-image:url()
- *          [obj.imgDesDir]:{string} image output dir,default:cssSrc
- *          [obj.layout]:{string} "linear"(default)|"matrix".matrix will use bin-packing
- *          [obj.hash]:{boolean} add hash flag on sprite image
+ *      spriteObj:{object}
+ *          spriteObj.cssSrc:{string} although you give content,we still need file name,so,give us cssSrc
+ *          [spriteObj.cssDesDir]:{string} css output dir ,default:cssSrc.we do not write new css file for
+ *                                                      you,you need do it yourself.we need it because we need to
+ *                                                      change css background-image:url()
+ *          [spriteObj.imgDesDir]:{string} image output dir,default:cssSrc
+ *          [spriteObj.layout]:{string} "linear"(default)|"matrix".matrix will use bin-packing
+ *          [spriteObj.hash]:{boolean} add hash flag on sprite image
  *  return:
  *      content:{buffer} new css file content
  *
  **/
 
-module.exports = sprite;
+module.exports.raw = sprite;
+
+var curWorkingDir = process.cwd();
+// Asynchronous-异步版
+module.exports.sprite = function ( spriteObj , callback ) {
+	fs.readFile( spriteObj.cssSrc , function ( err , buffer ) {
+		if ( err ) {
+			callback( err );
+		} else {
+			var content = sprite( buffer , spriteObj ) ,
+				cssRealOut = spriteObj.cssDesDir ?
+					path.join( curWorkingDir , spriteObj.cssDesDir ) :
+					path.join( curWorkingDir , path.dirname( spriteObj.cssSrc ) ) ,
+				baseName = path.basename( spriteObj.cssSrc );
+			fs.writeFile( cssRealOut + baseName , content , function ( err ) {
+				if ( err ) {
+					callback( err );
+				} else {
+					callback( null );
+				}
+			} );
+		}
+	} );
+};
+
+// Synchronous
+module.exports.spriteSync = function ( spriteObj ) {
+	var buffer = fs.readFileSync( spriteObj.cssSrc ) ,
+		content = sprite( buffer , spriteObj ) ,
+		cssRealOut = spriteObj.cssDesDir ?
+			path.join( curWorkingDir , spriteObj.cssDesDir ) :
+			path.join( curWorkingDir , path.dirname( spriteObj.cssSrc ) ) ,
+		baseName = path.basename( spriteObj.cssSrc );
+	fs.writeFileSync( cssRealOut + baseName , content );
+};
