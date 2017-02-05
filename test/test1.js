fd0329a27e2bfa065f756cfff46618fc45e752dc
qunwang <niuyiwangqun@sohu.com>
2017-02-05 12:42:10 +0800

    完善测试用例

--- test/test1.js
+++ test/test1.js
@@ -6,17 +6,21 @@ var sprite = require( '../index' );
 
 
 fs.readFile( 'css/base1/base.css' , function ( err , buffer ) {
+	var time = new Date();
 	if ( err ) {
 		console.log( err );
 	} else {
 		var content = sprite( buffer , {
 			cssSrc : 'css/base1/base.css' ,
 			cssDesDir : 'out' ,
-			imgDesDir : 'out'
+			imgDesDir : 'out' ,
+			hash : true
 		} );
 		fs.writeFile( 'out/result.css' , content , function ( err ) {
 			if ( err ) {
 				console.log( err );
+			} else {
+				console.log( 'finished ,spend ' + (new Date() - time) + 'ms' )
 			}
 		} );
 	}
