b55e2493f9a26aed653e029384caac3ca2d86e2c
qunwang <niuyiwangqun@sohu.com>
2017-02-08 22:44:04 +0800

    c

--- test/csso-raw.js
+++ test/csso-raw.js
@@ -4,9 +4,17 @@
 
 var gulp = require( "gulp" );
 var csso = require( "gulp-csso" );
-var srpite = require( "../index" ).raw;
+var sprite = require( "../index" ).sprite;
 
 
-gulp.src( "css/base1/base.css" , { base : "css" } )
-	.pipe( csso() )
-	.pipe( gulp.dest( "csso-css" ) );
+// gulp.src( "css/base1/base.css" , { base : "css" } )
+// 	.pipe( csso() )
+// 	.pipe( gulp.dest( "csso-css" ) );
+
+sprite( {
+	cssSrc : 'csso-css/base1/base.css' ,
+	cssDesDir : 'out' ,
+	imgDesDir : 'out'
+} , function () {
+	console.log( "end" );
+} );
